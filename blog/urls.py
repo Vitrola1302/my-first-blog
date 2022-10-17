49ba1dd7a1dd894bf7825fc9f4f176b038a618e3
pedro <phab@cesar.school>
2022-10-17 16:30:48 -0300

    Adicionando uma View e detalhando Posts

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path(route='', view = views.about, name='About'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
